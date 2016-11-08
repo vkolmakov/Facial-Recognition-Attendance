a4aaddfd0e41a485c268c25adfca84dd0ea68886
vkolmakov <vkolmakov@hotmail.com>
2016-11-08 16:42:39 -0600

    Refactor db connection and models

--- src/db.js
+++ src/db.js
@@ -2,40 +2,55 @@ import PouchDB from 'pouchdb'
 
 import { getOnly } from './util'
 
-let images = new PouchDB('FaceRecognition:images')
-let persons = new PouchDB('FaceRecognition:persons')
+class ImageModel {
+  constructor () {
+    this.DB_NAME = 'FaceRecognition:images'
+    this.images = new PouchDB(this.DB_NAME)
+  }
 
-export const Image = {
   getAll () {
-    return images.allDocs({ include_docs: true })
+    return this.images.allDocs({ include_docs: true })
       .then(result => result.rows.map(
         row => getOnly(row.doc, 'image', 'hash', 'identity', 'representation')
       ))
-  },
+  }
 
   save ({ image, hash, identity, representation }) {
     images.post({ image, hash, identity, representation })
-  },
-}
+  }
 
-export const Person = {
-  getAll () {
-    return persons.allDocs({ include_docs: true })
-      .then(result => result.rows.map(
-        row => getOnly(row.doc, 'id', 'name')
-      ))
-  },
-
-  save ({ id, name }) {
-    persons.post({ id, name })
+  drop () {
+    return this.images.destroy().then(
+      _ => this.images = new PouchDB(this.DB_NAME)
+    )
   }
 }
 
-export const dropAll = () => {
-  return Promise.all([images.destroy(), persons.destroy()])
-    .then(_ => {
-      images = new PouchDB('FaceRecognition:images')
-      persons = new PouchDB('FaceRecognition:persons')
-    })
+class PersonModel {
+  constructor () {
+    this.DB_NAME = 'FaceRecognition:persons'
+    this.persons = new PouchDB(this.DB_NAME)
+  }
+
+  getAll () {
+    return this.persons.allDocs({ include_docs: true })
+      .then(result => result.rows.map(
+        row => getOnly(row.doc, 'id', 'name')
+      ))
+  }
+
+  save ({ id, name }) {
+    return this.persons.post({ id, name })
+  }
+
+  drop () {
+    return this.persons.destroy().then(
+      _ => this.persons = new PouchDB(this.DB_NAME)
+    )
+  }
 }
 
+export const Person = new PersonModel()
+export const Image = new ImageModel()
+
+export const dropAll = () => Promise.all([Image.drop(), Person.drop()])
