3d641f9ae00bf873fcb935a8977364069fc992c3
vkolmakov <vkolmakov@hotmail.com>
2016-11-08 18:37:12 -0600

    Rename db connection prop in all models

--- src/db.js
+++ src/db.js
@@ -5,23 +5,23 @@ import { getOnly } from './util'
 class ImageModel {
   constructor () {
     this.DB_NAME = 'FaceRecognition:images'
-    this.images = new PouchDB(this.DB_NAME)
+    this.connection = new PouchDB(this.DB_NAME)
   }
 
   getAll () {
-    return this.images.allDocs({ include_docs: true })
+    return this.connection.allDocs({ include_docs: true })
       .then(result => result.rows.map(
         row => getOnly(row.doc, 'image', 'hash', 'identity', 'representation')
       ))
   }
 
   save ({ image, hash, identity, representation }) {
-    images.post({ image, hash, identity, representation })
+    this.connection.post({ image, hash, identity, representation })
   }
 
   drop () {
-    return this.images.destroy().then(
-      _ => this.images = new PouchDB(this.DB_NAME)
+    return this.connection.destroy().then(
+      _ => this.connection = new PouchDB(this.DB_NAME)
     )
   }
 }
@@ -29,23 +29,23 @@ class ImageModel {
 class PersonModel {
   constructor () {
     this.DB_NAME = 'FaceRecognition:persons'
-    this.persons = new PouchDB(this.DB_NAME)
+    this.connection = new PouchDB(this.DB_NAME)
   }
 
   getAll () {
-    return this.persons.allDocs({ include_docs: true })
+    return this.connection.allDocs({ include_docs: true })
       .then(result => result.rows.map(
         row => getOnly(row.doc, 'id', 'name')
       ))
   }
 
   save ({ id, name }) {
-    return this.persons.post({ id, name })
+    return this.connection.post({ id, name })
   }
 
   drop () {
-    return this.persons.destroy().then(
-      _ => this.persons = new PouchDB(this.DB_NAME)
+    return this.connection.destroy().then(
+      _ => this.connection = new PouchDB(this.DB_NAME)
     )
   }
 }
